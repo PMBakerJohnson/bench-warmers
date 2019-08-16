40de4e40d7e1c2ec78596a84212597dd4e464a50
pmbakerjohnson <xavier.richardson@onshoretechnology.local>
2019-08-16 16:01:55 -0500

    j

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { IndexComponent } from './index/index.component';
+import { UserPageComponent } from './user-page/user-page.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/index', pathMatch: 'full'},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
-  {path: 'index', component: IndexComponent}
+  {path: 'index', component: IndexComponent},
+  {path: 'user', component: UserPageComponent}
   
 ];
 
--- src/app/user-page/user-page.component.ts
+++ src/app/user-page/user-page.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserPageComponent implements OnInit {
 
+  username = "UserOne";
+
   constructor() { }
 
   ngOnInit() {
